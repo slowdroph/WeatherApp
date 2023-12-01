5dd3fcc76ee7e74c77bc035548aefab3efcaae24
caique <caiqueestrela538@gmail.com>
2023-11-30 22:22:22 -0300

    update main.js

--- main.js
+++ main.js
@@ -3,52 +3,49 @@
 const Weather = async function (city) {
     try {
         const request = await fetch(
-            `https://api.weatherapi.com/v1/current.json?key=0a9869fd42b14a2ba93235136232009&q&q=${city}&aqi=no`
+            `https://api.weatherapi.com/v1/current.json?key=0a9869fd42b14a2ba93235136232009&q=${city}&aqi=no`
         );
         const WeatherData = await request.json();
+        console.log(WeatherData);
 
         const temp = document.querySelectorAll(".data span")[0];
-        const cityElement = document.querySelectorAll(".data span")[1];
-        const wind = document.querySelector(
-            ".info-box[data-info='wind'] .font"
-        );
-        const humidity = document.querySelector(
-            ".info-box[data-info='humidity'] .font"
-        );
-        const errorMessage = document.querySelector(".erro");
-        const body = document.querySelector("body");
+        const cidade = document.querySelectorAll(".data span")[1];
+        const wind = document.querySelectorAll(".font")[2];
+        const humidity = document.querySelectorAll(".font")[0];
+        const ErrorMessage = document.querySelector(".erro");
+        const corpo = document.querySelector("body");
         const imagem = document.querySelector(".image");
-        const infoContainer = document.querySelector(".local-info-container");
+        const InfoContainer = document.querySelector(".local-info-container");
 
         if (request.status === 400) {
-            errorMessage.innerHTML = `Error: ${WeatherData?.error.message} ❌`;
-            errorMessage.style.display = "block";
+            ErrorMessage.innerHTML = `Error: ${WeatherData?.error.message} ❌`;
+            ErrorMessage.style.display = "block";
             document.querySelector(".hidden-container").classList.add("show");
-            infoContainer.classList.add("show");
+            InfoContainer.classList.add("show");
             throw new Error(`${WeatherData.error?.message}`);
         } else if (request.status === 200) {
-            errorMessage.innerHTML = "";
-            infoContainer.classList.remove("show");
+            ErrorMessage.innerHTML = "";
+            InfoContainer.classList.remove("show");
         }
 
-        body.style.backgroundImage = `url(nation/${WeatherData.location.country
+        corpo.style.backgroundImage = `url(nation/${WeatherData.location.country
             .split(" ")
             .join("")}.jpg)`;
 
-        infoContainer.innerHTML = `
-            <ul class="info-list">
-                <li><span class="highlights">City :</span> ${WeatherData.location.name}</li>
-                <li><span class="highlights">Region :</span> ${WeatherData.location.region}</li>
-                <li><span class="highlights">Country :</span> ${WeatherData.location.country}</li>
-            </ul>
-            <div>
-                <p><span class="highlights">Local Time :</span> ${WeatherData.location.localtime}</p>
-            </div> `;
+        InfoContainer.innerHTML = `
+        <ul class="info-list">
+            <li><span class="highlights">city :</span> ${WeatherData.location.name}</li>
+            <li><span class="highlights">region :</span> ${WeatherData.location.region}</li>
+            <li><span class="highlights">country :</span> ${WeatherData.location.country}</li>
+        </ul>
+        <div>
+            <p><span class="highlights">local time :</span> ${WeatherData.location.localtime}</p>
+        </div> `;
 
-        infoContainer.style.visibility = "visible";
+        InfoContainer.style.visibility = "visible";
 
         temp.innerHTML = Number.parseInt(WeatherData.current.temp_c) + " °";
-        cityElement.innerHTML = WeatherData.location.name;
+        cidade.innerHTML = WeatherData.location.name;
 
         humidity.innerHTML = WeatherData.current.humidity + " %";
         wind.innerHTML =
@@ -72,20 +69,20 @@ const Weather = async function (city) {
 };
 
 const form = document.querySelector("#texto");
-const button = document.querySelector(".button");
-const infoModal = document.querySelector(".hidden-container");
+const botão = document.querySelector(".button");
+const InfoModal = document.querySelector(".hidden-container");
 
-button.addEventListener("click", getValue);
+botão.addEventListener("click", Value);
 form.addEventListener("keydown", function (e) {
     if (e.code === "Enter") {
-        getValue();
+        Value();
     }
 });
 
-function getValue() {
+function Value() {
     Weather(form.value);
-    if (infoModal.classList.contains("show")) {
-        infoModal.classList.remove("show");
+    if (InfoModal.classList.contains("show")) {
+        InfoModal.classList.remove("show");
     } else {
         form.classList.add("hidden");
     }
