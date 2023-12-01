7b163e79158125ffc6eb248c0e0823d36611da78
caique <caiqueestrela538@gmail.com>
2023-11-30 21:19:33 -0300

    compressed images

--- main.js
+++ main.js
@@ -3,32 +3,52 @@
 const Weather = async function (city) {
     try {
         const request = await fetch(
-            `https://api.weatherapi.com/v1/current.json?key=0a9869fd42b14a2ba93235136232009&q=${city}&aqi=no`
+            `https:api.weatherapi.com/v1/current.json?key=0a9869fd42b14a2ba93235136232009&q=${city}&aqi=no`
         );
         const WeatherData = await request.json();
-        console.log(WeatherData);
 
         const temp = document.querySelectorAll(".data span")[0];
-        const cidade = document.querySelectorAll(".data span")[1];
-        const wind = document.querySelectorAll(".font")[2];
-        const humidity = document.querySelectorAll(".font")[0];
-        const ErrorMessage = document.querySelector(".erro");
-        const corpo = document.querySelector("body");
+        const cityElement = document.querySelectorAll(".data span")[1];
+        const wind = document.querySelector(
+            ".info-box[data-info='wind'] .font"
+        );
+        const humidity = document.querySelector(
+            ".info-box[data-info='humidity'] .font"
+        );
+        const errorMessage = document.querySelector(".erro");
+        const body = document.querySelector("body");
         const imagem = document.querySelector(".image");
+        const infoContainer = document.querySelector(".local-info-container");
 
         if (request.status === 400) {
-            ErrorMessage.innerHTML = `Error: ${WeatherData?.error.message} ❌`;
-            ErrorMessage.style.display = "block";
+            errorMessage.innerHTML = `Error: ${WeatherData?.error.message} ❌`;
+            errorMessage.style.display = "block";
             document.querySelector(".hidden-container").classList.add("show");
-            throw new Error(`${WeatherData.error?.message}`);
+            infoContainer.classList.add("show");
+            throw new Error(`${weatherData.error?.message}`);
         } else if (request.status === 200) {
-            ErrorMessage.innerHTML = "";
+            errorMessage.innerHTML = "";
+            infoContainer.classList.remove("show");
         }
 
-        corpo.style.backgroundImage = `url(nation/${(WeatherData.location.country).split(" ").join("")}.jpg)`;
+        body.style.backgroundImage = `url(nation/${WeatherData.location.country
+            .split(" ")
+            .join("")}.jpg)`;
+
+        infoContainer.innerHTML = `
+            <ul class="info-list">
+                <li><span class="highlights">City :</span> ${WeatherData.location.name}</li>
+                <li><span class="highlights">Region :</span> ${WeatherData.location.region}</li>
+                <li><span class="highlights">Country :</span> ${WeatherData.location.country}</li>
+            </ul>
+            <div>
+                <p><span class="highlights">Local Time :</span> ${WeatherData.location.localtime}</p>
+            </div> `;
+
+        infoContainer.style.visibility = "visible";
 
         temp.innerHTML = Number.parseInt(WeatherData.current.temp_c) + " °";
-        cidade.innerHTML = WeatherData.location.name;
+        cityElement.innerHTML = WeatherData.location.name;
 
         humidity.innerHTML = WeatherData.current.humidity + " %";
         wind.innerHTML =
@@ -52,20 +72,20 @@ const Weather = async function (city) {
 };
 
 const form = document.querySelector("#texto");
-const botão = document.querySelector(".button");
-const InfoModal = document.querySelector(".hidden-container");
+const button = document.querySelector(".button");
+const infoModal = document.querySelector(".hidden-container");
 
-botão.addEventListener("click", Value);
+button.addEventListener("click", getValue);
 form.addEventListener("keydown", function (e) {
     if (e.code === "Enter") {
-        Value();
+        getValue();
     }
 });
 
-function Value() {
+function getValue() {
     Weather(form.value);
-    if (InfoModal.classList.contains("show")) {
-        InfoModal.classList.remove("show");
+    if (infoModal.classList.contains("show")) {
+        infoModal.classList.remove("show");
     } else {
         form.classList.add("hidden");
     }
